d47731ca60991ecc1156bb62381cc637d6d69c41
Зябкин Владимир <vovvovin5124@gmail.com>
2025-12-07 21:25:33 +0700

    usecase + handler

--- backend/internal/adapter/in/http/auth_handler.go
+++ backend/internal/adapter/in/http/auth_handler.go
@@ -10,8 +10,9 @@ import (
 )
 
 type AuthHandler struct {
-	log    *slog.Logger
-	AuthUC *usecase.GoogleAuthUC
+	log       *slog.Logger
+	AuthUC    *usecase.GoogleAuthUC
+	RefreshUC *usecase.RefreshTokenUC
 }
 
 func NewAuthHandler(
@@ -46,4 +47,28 @@ func (h *AuthHandler) GoogleAuth(ctx *gin.Context) {
 		slog.String("user_id", resp.User.ID.String()))
 
 	ctx.JSON(http.StatusCreated, resp)
+}
+
+func (h *AuthHandler) RefreshToken(ctx *gin.Context) {
+	var req dto.RefreshTokenRequest
+	if err := ctx.ShouldBindJSON(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": "invalid json"})
+		return
+	}
+
+	resp, err := h.RefreshUC.Execute(ctx, req)
+	if err != nil {
+		status, msg, internalErr := HttpError(err)
+		h.log.ErrorContext(ctx, "failed to refresh tokens",
+			slog.Int("status", status),
+			slog.String("public_msg", msg),
+			slog.Any("cause", internalErr))
+		ctx.JSON(status, gin.H{"error": msg})
+		return
+	}
+
+	h.log.InfoContext(ctx, "successful tokens refresh",
+		slog.String("user_id", resp.User.ID.String()))
+
+	ctx.JSON(http.StatusOK, resp)
 }
