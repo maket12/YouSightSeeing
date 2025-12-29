01906535ce82115d9e3b0aed209c35f991a91bb4
Kirpich Mira <k.maltsev@g.nsu.ru>
2025-12-29 23:53:15 +0700

    added a  mapper to the uscase

--- backend/internal/app/usecase/search_places_uc.go
+++ backend/internal/app/usecase/search_places_uc.go
@@ -2,6 +2,7 @@ package usecase
 
 import (
 	"YouSightSeeing/backend/internal/app/dto"
+	"YouSightSeeing/backend/internal/app/mappers"
 	"YouSightSeeing/backend/internal/app/uc_errors"
 	"YouSightSeeing/backend/internal/domain/entity"
 	"YouSightSeeing/backend/internal/domain/port"
@@ -42,16 +43,7 @@ func (uc *SearchPlacesUC) Execute(ctx context.Context, req dto.SearchPlacesReque
 		return dto.SearchPlacesResponse{}, uc_errors.Wrap(uc_errors.ErrSearchPlacesFailed, err)
 	}
 
-	respPlaces := make([]dto.Place, 0, len(places))
-	for _, p := range places {
-		respPlaces = append(respPlaces, dto.Place{
-			Name:        p.Name,
-			Address:     p.Address,
-			Categories:  p.Categories,
-			Coordinates: p.Coordinates,
-			PlaceID:     p.ID,
-		})
-	}
-
-	return dto.SearchPlacesResponse{Places: respPlaces}, nil
+	return dto.SearchPlacesResponse{
+		Places: mappers.MapPlacesIntoResponse(places),
+	}, nil
 }
